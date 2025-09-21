fb625092d88d58ee38c16518eed79a372af85090
mj-52 <johnmuthigani52@gmail.com>
2025-09-22 08:16:15 +1000

    Update main_rm2.py

--- main_rm2.py
+++ main_rm2.py
@@ -21,7 +21,7 @@ ssid = os.getenv("PO_SSID")
 demo = True
 
 # Bot Settings
-min_payout = 80
+min_payout = 60
 period = 300
 expiration = 300
 INITIAL_AMOUNT = 1
@@ -281,6 +281,7 @@ def main_trading_loop():
 
 if __name__ == "__main__":
     main_trading_loop()
+
 
 
 
